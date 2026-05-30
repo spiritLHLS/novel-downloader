62f5a0d2c86df28c5220975b9a8cf224f31844bf
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2026-05-30 15:13:05 +0800

    fix；升级Action的组件版本，修复兼容性处理

--- src/novel_downloader/infra/i18n.py
+++ src/novel_downloader/infra/i18n.py
@@ -15,9 +15,15 @@ from novel_downloader.infra.persistence.state import state_mgr
 
 def get_translation(lang: str) -> gettext.NullTranslations:
     try:
-        mo_path = files("novel_downloader.locales").joinpath(
-            lang, "LC_MESSAGES", "messages.mo"
-        )
+        locales = files("novel_downloader.locales")
+        try:
+            mo_path = locales.joinpath(lang, "LC_MESSAGES", "messages.mo")
+        except TypeError:
+            mo_path = (
+                locales.joinpath(lang)
+                .joinpath("LC_MESSAGES")
+                .joinpath("messages.mo")
+            )
         with mo_path.open("rb") as f:
             return gettext.GNUTranslations(f)
     except (FileNotFoundError, ModuleNotFoundError):
